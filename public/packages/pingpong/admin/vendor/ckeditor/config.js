bc4dcad69a41296b335d8afb77c3648945f8b81b
esab <esab.dev@gmail.com>
2016-06-15 17:15:16 +0300

    admin ckeditor config

--- public/packages/pingpong/admin/vendor/ckeditor/config.js
+++ public/packages/pingpong/admin/vendor/ckeditor/config.js
@@ -3,7 +3,9 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function( config )
+{
+    config.removePlugins = 'forms,font,print,save,language,newpage,selectall,smiley,table,bidi,div,flash,about,iframe,preview';
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
