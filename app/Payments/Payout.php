63cb07769949b2d28c51e444ee0aa93d51f042cd
esab <esab.dev@gmail.com>
2016-06-28 19:22:04 +0300

    Payout

---
+++ app/Payments/Payout.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: esabbath
+ * Date: 6/28/16
+ * Time: 7:15 PM
+ */
+
+namespace App\Payments;
+
+
+use App\Project;
+use App\User;
+use Illuminate\Support\Facades\Config;
+use PayPal\Api\Currency;
+use PayPal\Api\PayoutItem;
+use PayPal\Api\PayoutSenderBatchHeader;
+
+class Payout
+{
+	private $_apiContext;
+
+	public function __construct()
+	{
+		// setup PayPal api context
+		$paypal_conf = Config::get('paypal');
+		$this->_apiContext = new ApiContext(new OAuthTokenCredential($paypal_conf['client_id'], $paypal_conf['secret']));
+		$this->_apiContext->setConfig($paypal_conf['settings']);
+	}
+
+	public function sendMoney(User $user, Project $project)
+	{
+		$payout = new \PayPal\Api\Payout();
+
+		$senderBatchHeader = new PayoutSenderBatchHeader();
+
+		$senderBatchHeader->setSenderBatchId(uniqid())
+			->setEmailSubject("You have a Payout!");
+
+		// #### Sender Item
+		// Please note that if you are using single payout with sync mode, you can only pass one Item in the request
+		$senderItem = new PayoutItem();
+		$senderItem->setRecipientType('Email')
+			->setNote('Thanks for your patronage!')
+			->setReceiver($user->email)
+			->setSenderItemId("2014031400023")
+			->setAmount(new Currency('{
+                        "value":"1.0",
+                        "currency":"USD"
+                    }'));
+		$payout->setSenderBatchHeader($senderBatchHeader)
+			->addItem($senderItem);
+// For Sample Purposes Only.
+		$request = clone $payout;
+// ### Create Payout
+		try {
+			$output = $payout->createSynchronous($this->_apiContext);
+		} catch (Exception $ex) {
+			// NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+			ResultPrinter::printError("Created Single Synchronous Payout", "Payout", null, $request, $ex);
+			exit(1);
+		}
+// NOTE: PLEASE DO NOT USE RESULTPRINTER CLASS IN YOUR ORIGINAL CODE. FOR SAMPLE ONLY
+		ResultPrinter::printResult("Created Single Synchronous Payout", "Payout", $output->getBatchHeader()->getPayoutBatchId(), $request, $output);
+		return $output;
+	}
+}
